f8c5391a8619886fdd0ebf93a9ee2cc776439a38
Liran-tal <taliran44@gmail.com>
2022-03-20 15:32:09 +0200

    LA-41: add list of selectors to be scraped in LinkedIn

--- query-selectors.js
+++ query-selectors.js
@@ -1,45 +1,45 @@
-<!-- URL of search: ?keywords=stealth, filter: people -->
+// URL of search: ?keywords=stealth, filter: people 
 await
 page.goto('https://www.linkedin.com/search/results/people/?geoUrn=%5B%22101620260%22%5D&keywords=stealth%20Co-Founder%20ceo%20cto&origin=FACETED_SEARCH&sid=7kR');
-<!-- checked -->
+// checked 
 
-<!-- link to profile (retrieve href from node list) -->
+// link to profile (retrieve href from node list) 
 document.querySelectorAll('span.entity-result__title-line.entity-result__title-line--2-lines > span > a')
-<!-- checked -->
+// checked 
 
-<!-- profile picture -->
+// profile picture 
 document.querySelector('div > div > div > button > img').src;
-<!-- checked -->
+// checked 
 
-<!-- Name -->
+// Name 
 document.querySelector('h1');
-<!-- checked -->
+// checked 
 
-<!-- title -->
+// title 
 document.querySelector('div.text-body-medium.break-words').innerText;
-<!-- checked -->
+// checked 
 
 
-<!-- contact info button -->
+// contact info button 
 document.querySelector('#top-card-text-details-contact-info').href;
-<!-- checked -->
+// checked 
 
 
-<!-- linkedin profile -->
+// linkedin profile 
 document.querySelector('div > section.pv-contact-info__contact-type.ci-vanity-url > div > a').href;
-<!-- checked -->
+// checked 
 
 
-<!-- website -->
+// website 
 document.querySelector('section.pv-contact-info__contact-type.ci-websites > ul > li > div > a').href;
-<!-- checked -->
+// checked 
 
 
-<!-- close modal button -->
+// close modal button 
 document.querySelector('div[role=dialog] > button');
-<!-- checked -->
+// checked 
 
 
-<!-- next page in list -->
+// next page in list 
 document.querySelector('div.artdeco-pagination.artdeco-pagination--has-controls > button[aria-label=Next');
-<!-- checked -->
+// checked 
