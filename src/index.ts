76e198040025e28dd7b53ad123256521de3a9ad8
Liran-tal <taliran44@gmail.com>
2022-03-20 17:27:04 +0200

    change server connenction message in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { app } from './app';
 
 const PORT = process.env.PORT || 8080;
-const SERVER_MESSAGE = `Express listens on http://127.0.0.1:${PORT}`;
+const SERVER_MESSAGE = process.env.PORT
+  ? `Express listens on https:<NameOfDomain>:${PORT}`
+  : `Express listens on http://127.0.0.1:${PORT}`;
 
 // tslint:disable-next-line: no-console
 app.listen(PORT, () => console.log(SERVER_MESSAGE));
