40fca93fb36d3f490ec1bb93e168896aa791ce1f
Liran-tal <taliran44@gmail.com>
2022-04-19 17:55:53 +0300

    add demo functions for air table

--- src/airTable/airTablePipe.ts
+++ src/airTable/airTablePipe.ts
@@ -7,3 +7,35 @@ config({ path: path.resolve(__dirname, '../.env') });
 
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appq2ntvhHq30MiT8');
 
+base('Table 1').select({
+  // Selecting the first 3 records in Grid view:
+  maxRecords: 3,
+  view: "Grid view",
+  fields: ["fldg01YA673VBUzFb", "fldMFOfCBcfw3TVLz"],
+}).eachPage(function page(records, fetchNextPage) {
+  // This function (`page`) will get called for each page of records.
+
+  records.forEach(function(record) {
+      console.log('Retrieved', record.get("test field 1"));
+  });
+
+  // To fetch the next page of records, call `fetchNextPage`.
+  // If there are more records, `page` will get called again.
+  // If there are no more records, `done` will get called.
+  fetchNextPage();
+
+}, function done(err) {
+  if (err) { 
+    console.error(err); return; 
+  }
+});
+
+
+base('Table 1').find('recZleghsupzhi1hF', function(err, record) {
+  if (record) { 
+    console.log('Retrieved', record.id);
+    return; 
+  }
+
+  console.error(err); 
+});
