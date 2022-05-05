e8baca12396928479ee86f3596d78ce7b02b8873
julie-chernen <juliechernen@gmail.com>
2022-05-05 22:46:18 +0300

    frontend url is added to cors

--- config.ts
+++ config.ts
@@ -1,10 +1,4 @@
 export const corsConfig = {
-  origin: [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    'http://appleseedsTodo.herokuapp.com',
-    'https://appleseedsTodo.herokuapp.com',
-    `https://linkdin-auto-bot-back.herokuapp.com`,
-  ],
+  origin: ['http://localhost:3000', 'https://velocity-ventures-linkedin-autobot.netlify.app/'],
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
 };
