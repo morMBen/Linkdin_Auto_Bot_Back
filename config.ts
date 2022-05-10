b8a33ddb7837264bf0bcbc9c1ae445c2904a9426
Liran-tal <taliran44@gmail.com>
2022-05-10 10:34:35 +0300

    add options to cors config and change uri of netlify

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const corsConfig = {
-  origin: ['http://localhost:3000', 'https://velocity-ventures-linkedin-autobot.netlify.app/'],
-  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  origin: ['http://localhost:3000', 'https://velocity-ventures-linkedin-autobot.netlify.app'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
 };
